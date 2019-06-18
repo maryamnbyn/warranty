49ee45d828fada864b1cfd74bf32a9c13ce7fc03
maryam nabiyan <maryam.nbyn@gmail.com>
2019-06-18 15:59:23 +0430

    login argon form in login admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,204 @@
-@extends('layouts.app')
+@include('panel.head')
 
-@section('title')
-    ورود
-@endsection
+<body class="bg-default">
+<div class="main-content">
+    <!-- Navbar -->
+    <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
+        <div class="container px-4">
+            <a class="navbar-brand" href="../index.html">
+                <img src="../assets/img/brand/white.png" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar-collapse-main">
+                <!-- Collapse header -->
+                <div class="navbar-collapse-header d-md-none">
+                    <div class="row">
+                        <div class="col-6 collapse-brand">
+                            <a href="../index.html">
+                                <img src="../assets/img/brand/blue.png">
+                            </a>
+                        </div>
+                        <div class="col-6 collapse-close">
+                            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- Navbar items -->
 
-@section('content')
-    <div class="container">
+            </div>
+        </div>
+    </nav>
+    <!-- Header -->
+    <div class="header bg-gradient-primary py-7 py-lg-8">
+
+        <div class="separator separator-bottom separator-skew zindex-100">
+            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
+        </div>
+    </div>
+    <!-- Page content -->
+    <div class="container mt--8 pb-5">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header"><b>{{ __('ورود') }}</b></div>
+            <div class="col-lg-5 col-md-7">
+                <div class="card bg-secondary shadow border-0">
 
-                    <div class="card-body">
+                    <div class="card-body px-lg-5 py-lg-5">
+
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-
-                            <div class="form-group row">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('آدرس ایمیل:') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
+                            <div class="form-group mb-3">
+                                <div class="input-group input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="ایمیل" name="email" type="email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="پسورد" name="password" type="password">
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('رمز عبور') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="current-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('ورود') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('آیا پسورد خود را فراموش کرده اید؟!') }}
-                                        </a>
-                                    @endif
-                                </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary my-4">ورود</button>
                             </div>
                         </form>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
-@endsection
+</div>
+<!-- Footer -->
+
+<!-- Argon Scripts -->
+<!-- Core -->
+<script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
+<script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Argon JS -->
+<script src="../assets/js/argon.js?v=1.0.0"></script>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{--@extends('layouts.app')--}}
+
+{{--@section('title')--}}
+    {{--ورود--}}
+{{--@endsection--}}
+
+{{--@section('content')--}}
+    {{--<div class="container">--}}
+        {{--<div class="row justify-content-center">--}}
+            {{--<div class="col-md-8">--}}
+                {{--<div class="card">--}}
+                    {{--<div class="card-header"><b>{{ __('ورود') }}</b></div>--}}
+
+                    {{--<div class="card-body">--}}
+                        {{--<form method="POST" action="{{ route('login') }}">--}}
+                            {{--@csrf--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<label for="email"--}}
+                                       {{--class="col-md-4 col-form-label text-md-right">{{ __('آدرس ایمیل:') }}</label>--}}
+
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="email" type="email"--}}
+                                           {{--class="form-control @error('email') is-invalid @enderror" name="email"--}}
+                                           {{--value="{{ old('email') }}" required autocomplete="email" autofocus>--}}
+
+                                    {{--@error('email')--}}
+                                    {{--<span class="invalid-feedback" role="alert">--}}
+                                        {{--<strong>{{ $message }}</strong>--}}
+                                    {{--</span>--}}
+                                    {{--@enderror--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<label for="password"--}}
+                                       {{--class="col-md-4 col-form-label text-md-right">{{ __('رمز عبور') }}</label>--}}
+
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="password" type="password"--}}
+                                           {{--class="form-control @error('password') is-invalid @enderror" name="password"--}}
+                                           {{--required autocomplete="current-password">--}}
+
+                                    {{--@error('password')--}}
+                                    {{--<span class="invalid-feedback" role="alert">--}}
+                                        {{--<strong>{{ $message }}</strong>--}}
+                                    {{--</span>--}}
+                                    {{--@enderror--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<div class="col-md-6 offset-md-4">--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row mb-0">--}}
+                                {{--<div class="col-md-8 offset-md-4">--}}
+                                    {{--<button type="submit" class="btn btn-primary">--}}
+                                        {{--{{ __('ورود') }}--}}
+                                    {{--</button>--}}
+
+                                    {{--@if (Route::has('password.request'))--}}
+                                        {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
+                                            {{--{{ __('آیا پسورد خود را فراموش کرده اید؟!') }}--}}
+                                        {{--</a>--}}
+                                    {{--@endif--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</form>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--@endsection--}}
