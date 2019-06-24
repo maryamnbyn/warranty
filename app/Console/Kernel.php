45987d4eb106bd0c9b641686250271116e3a9ca3
maryam nabiyan <maryam.nbyn@gmail.com>
2019-06-24 11:59:56 +0430

    add scaduale queue:work in project

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('queue:run')
+         $schedule->command('queue:work')
                   ->everyMinute();
     }
 
