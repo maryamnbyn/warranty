d3a888ee80a618ac48c0abf32d403a3c995e9375
maryamnbyn <maryam.nbyn@gmail.com>
2019-06-26 16:56:57 +0430

    delete scaduale

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('queue:work')
-                  ->everyMinute();
+        // $schedule->command('inspire')
+        //          ->hourly();
     }
 
     /**
